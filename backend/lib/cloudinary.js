396748ef07b10cdc18d44765a762c602fa5ca7f1
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-11-21 17:34:49 +0530

    cloudinary config and upload to cloudinary method

---
+++ backend/lib/cloudinary.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadToCloudinary = async (file) => {
+  try {
+    const result = await cloudinary.uploader.upload(file.tempFilePath, {
+      resource_type: "auto",
+    });
+    return result.secure_url;
+  } catch (error) {
+    console.log("Error in uploadToCloudinary method " + error);
+    throw new Error("Error uploading to cloudinary " + error);
+  }
+};
+
+export default cloudinary;
