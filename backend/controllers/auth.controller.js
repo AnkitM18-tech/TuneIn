9c2fef327638c7638cc3b0b0ecd69729b6071479
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-11-21 17:34:25 +0530

    authentication callback

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,27 @@
+import { User } from "../models/user.models.js";
+
+export const authCallback = async (req, res, next) => {
+  try {
+    // clerk's way of storing fields
+    const { id, firstName, lastName, imageUrl } = req.body;
+
+    // check if user already exists
+    const user = await User.findOne({ clerkId: id });
+    if (!user) {
+      // signup
+      await User.create({
+        clerkId: id,
+        fullName: `${firstName} ${lastName}`,
+        imageUrl,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Auth Callback successful",
+    });
+  } catch (error) {
+    console.log("Error in auth callback ", error);
+    next(error);
+  }
+};
