ab0db1750382c6bdf9a1b41e813d578fa0e43c29
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-11-21 17:36:05 +0530

    file upload and global error handling logic middlewares

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import { clerkMiddleware } from "@clerk/express";
+import fileUpload from "express-fileupload";
+import path from "path";
 
 import userRouter from "../routes/user.routes.js";
 import adminRouter from "../routes/admin.routes.js";
@@ -12,8 +15,23 @@ import { connectDB } from "../lib/db.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use(clerkMiddleware()); // adds auth to req object => req.auth
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+      fileSize: 5 * 1024 * 1024, //5 MB max file size
+    },
+  })
+);
 
 app.use("/api/users", userRouter);
 app.use("/api/admin", adminRouter);
@@ -21,6 +39,17 @@ app.use("/api/auth", authRouter);
 app.use("/api/songs", songsRouter);
 app.use("/api/albums", albumsRouter);
 app.use("/api/stats", statsRouter);
+
+// global error handler
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    success: false,
+    message:
+      process.env.NODE_ENV === "development"
+        ? err.message
+        : "Internal Server Error",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
