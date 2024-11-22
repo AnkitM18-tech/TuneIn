4f3d18bcdc22345a41837f5f2257b77b2a87dba0
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-11-22 14:12:33 +0530

    Auth Provider and Authcallback page

---
+++ frontend/src/components/SignInOAuthButton.tsx
@@ -0,0 +1,27 @@
+import { useSignIn } from "@clerk/clerk-react";
+import { Button } from "./ui/button";
+
+const SignInOAuthButton = () => {
+  const { signIn, isLoaded } = useSignIn();
+  const signInWithGoogle = () => {
+    signIn?.authenticateWithRedirect({
+      strategy: "oauth_google",
+      redirectUrl: "/sso-callback",
+      redirectUrlComplete: "/auth-callback",
+    });
+  };
+  if (!isLoaded) {
+    return null;
+  }
+  return (
+    <Button
+      onClick={signInWithGoogle}
+      variant={"secondary"}
+      className="w-full text-white border-zinc-200 h-11"
+    >
+      Continue With Google
+    </Button>
+  );
+};
+
+export default SignInOAuthButton;
