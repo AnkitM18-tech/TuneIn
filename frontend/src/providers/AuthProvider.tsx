dfbbfba26324df9fb5e7c6c7f5611738acab7ff8
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-11-23 23:52:37 +0530

    check admin status

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -1,4 +1,5 @@
 import { axiosInstance } from "@/lib/axios";
+import { useAuthStore } from "@/stores/useAuthStore";
 import { useAuth } from "@clerk/clerk-react";
 import { Loader } from "lucide-react";
 import { useEffect, useState } from "react";
@@ -13,6 +14,7 @@ const updateApiToken = (token: string | null) => {
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const { getToken } = useAuth();
   const [loading, setLoading] = useState(true);
+  const { checkAdminStatus } = useAuthStore();
 
   useEffect(() => {
     const initAuth = async () => {
@@ -21,6 +23,7 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         updateApiToken(token);
 
         if (token) {
+          await checkAdminStatus();
         }
       } catch (error) {
         updateApiToken(null);
