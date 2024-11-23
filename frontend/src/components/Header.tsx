ee35aa5bb7bf31d9f4872b238d0d4825961d2d5e
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-11-24 00:06:18 +0530

    Logo added and styling modifications

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,26 +1,35 @@
-import { SignedIn, SignedOut, SignOutButton } from "@clerk/clerk-react";
+import { SignedOut, UserButton } from "@clerk/clerk-react";
 import { LayoutDashboardIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 import SignInOAuthButton from "./SignInOAuthButton";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
 
 const Header = () => {
-  const isAdmin = false;
+  const { isAdmin } = useAuthStore();
   return (
-    <div className="sticky top-0 z-10 flex items-center justify-between p-4 bg-zinc-900/75 backdrop-blur-md">
-      <div className="flex items-center gap-2">TuneIn</div>
+    <div className="sticky top-0 z-10 flex items-center justify-between p-4 rounded-md bg-zinc-900/75 backdrop-blur-md">
+      <div className="flex items-center gap-2">
+        <img src={"/tunein.png"} alt={"Tune In Logo"} className="size-8" />
+        TuneIn
+      </div>
       <div className="flex items-center gap-4">
         {isAdmin && (
-          <Link to="/admin">
+          <Link
+            to="/admin"
+            className={cn(buttonVariants({ variant: "outline" }))}
+          >
             <LayoutDashboardIcon className="mr-2 size-4" />
             Admin Dashboard
           </Link>
         )}
-        <SignedIn>
-          <SignOutButton />
-        </SignedIn>
+
         <SignedOut>
           <SignInOAuthButton />
         </SignedOut>
+
+        <UserButton />
       </div>
     </div>
   );
