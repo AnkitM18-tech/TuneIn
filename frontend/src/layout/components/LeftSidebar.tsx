aeb1494010d0df23efedcaa30baa39b2fdbf5e09
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-11-23 16:13:25 +0530

    Left Sidebar

---
+++ frontend/src/layout/components/LeftSidebar.tsx
@@ -0,0 +1,93 @@
+import PlaylistSkeleton from "@/components/skeletons/PlaylistSkeleton";
+import { buttonVariants } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { cn } from "@/lib/utils";
+import { useMusicStore } from "@/stores/useMusicStore";
+import { SignedIn } from "@clerk/clerk-react";
+import { HomeIcon, Library, MessageCircle } from "lucide-react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const LeftSidebar = () => {
+  const { isLoading, albums, fetchAlbums } = useMusicStore();
+
+  useEffect(() => {
+    fetchAlbums();
+  }, [fetchAlbums]);
+
+  return (
+    <div className="flex flex-col h-full gap-2">
+      {/* Navigation Menu */}
+      <div className="p-4 rounded-lg bg-zinc-900">
+        <div className="space-y-2">
+          <Link
+            to="/"
+            className={cn(
+              buttonVariants({
+                variant: "ghost",
+                className: "w-full justify-start text-white hover:bg-zinc-800",
+              })
+            )}
+          >
+            <HomeIcon className="mr-2 size-5" />
+            <span className="hidden md:inline">Home</span>
+          </Link>
+          <SignedIn>
+            <Link
+              to="/chat"
+              className={cn(
+                buttonVariants({
+                  variant: "ghost",
+                  className:
+                    "w-full justify-start text-white hover:bg-zinc-800",
+                })
+              )}
+            >
+              <MessageCircle className="mr-2 size-5" />
+              <span className="hidden md:inline">Messages</span>
+            </Link>
+          </SignedIn>
+        </div>
+      </div>
+
+      {/* Library Section */}
+      <div className="flex-1 p-4 rounded-lg bg-zinc-900">
+        <div className="flex items-center justify-between mb-4">
+          <div className="flex items-center px-2 text-white">
+            <Library className="mr-2 size-5" />
+            <span className="hidden md:inline-block">Playlists</span>
+          </div>
+        </div>
+        <ScrollArea className="h-[calc(100vh-300px)]">
+          <div className="space-y-2">
+            {isLoading ? (
+              <PlaylistSkeleton />
+            ) : (
+              albums?.map((album) => (
+                <Link
+                  to={`/albums/${album._id}`}
+                  key={album._id}
+                  className="flex items-center gap-3 p-2 rounded-md cursor-pointer hover:bg-zinc-800 group"
+                >
+                  <img
+                    src={album.imageUrl}
+                    alt={album.title}
+                    className="flex-shrink-0 object-cover rounded-md size-12"
+                  />
+                  <div className="flex-1 hidden min-w-0 md:block">
+                    <p className="font-medium truncate">{album.title}</p>
+                    <p className="truncate font-sm text-zinc-400">
+                      Album {">"} {album.artist}
+                    </p>
+                  </div>
+                </Link>
+              ))
+            )}
+          </div>
+        </ScrollArea>
+      </div>
+    </div>
+  );
+};
+
+export default LeftSidebar;
