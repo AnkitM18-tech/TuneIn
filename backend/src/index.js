612401e6d830b912ae54deafe2ab01b3b2e3d4e4
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-11-26 00:26:21 +0530

    socket io integrated

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import { clerkMiddleware } from "@clerk/express";
 import fileUpload from "express-fileupload";
 import path from "path";
 import cors from "cors";
+import { createServer } from "http";
 
 import userRouter from "../routes/user.routes.js";
 import adminRouter from "../routes/admin.routes.js";
@@ -13,12 +14,16 @@ import albumsRouter from "../routes/albums.routes.js";
 import statsRouter from "../routes/stats.routes.js";
 
 import { connectDB } from "../lib/db.js";
+import { initializeSocket } from "../lib/socket.js";
 
 dotenv.config();
 
 const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const httpServer = createServer(app);
+initializeSocket(httpServer);
 
 app.use(
   cors({
@@ -59,7 +64,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
