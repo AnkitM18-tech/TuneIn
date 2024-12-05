108fec34c795db3fd86adb587377c571e659539f
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-12-06 02:15:01 +0530

    formatTime method changed

--- frontend/src/pages/chat/ChatPage.tsx
+++ frontend/src/pages/chat/ChatPage.tsx
@@ -25,11 +25,17 @@ const NoConversationPlaceholder = () => (
 );
 
 const formatTime = (date: string) => {
-  return new Date(date).toLocaleTimeString("en-US", {
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: true,
-  });
+  const messageTime =
+    new Date(date).toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    }) +
+    " - " +
+    new Date(date).toLocaleDateString("en-US", {
+      dateStyle: "medium",
+    });
+  return messageTime;
 };
 
 const ChatPage = () => {
